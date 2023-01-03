93757c2a1a8f5a89c69efe14511dd1c979508f2c
Piotr Mol <piotr94mol@gmail.com>
2023-01-03 09:43:43 +0100

    feat: subscriptions starter

---
+++ index.js
@@ -0,0 +1,54 @@
+import { ApolloServer } from '@apollo/server';
+import { createServer } from 'http';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import bodyParser from 'body-parser';
+import express from 'express';
+
+const port = 3000;
+
+const typeDefs = `
+    type Query {
+        foo: String!
+    }
+    type Mutation {
+        scheduleOperation(name: String!): String!
+    }
+`;
+
+const resolvers = {
+    Mutation: {
+        scheduleOperation(_, { name }) {
+            console.log(`Mocking operation: ${name}`);
+            return `Operation: ${name} scheduled!`;
+        }
+    }, 
+    Query: {
+        foo() {
+            return 'foo';
+        }
+    }
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const app = express();
+const httpServer = createServer(app);
+
+const apolloServer = new ApolloServer({
+    schema,
+    plugins: [
+       // Proper shutdown for the HTTP server.
+       ApolloServerPluginDrainHttpServer({ httpServer }),
+    ]
+});
+
+await apolloServer.start();
+
+app.use('/graphql', bodyParser.json(), expressMiddleware(apolloServer));
+
+httpServer.listen(port, () => {
+    console.log(`🚀 Query endpoint ready at http://localhost:${port}/graphql`);
+    console.log(`🚀 Subscription endpoint ready at ws://localhost:${port}/graphql`);
+});
